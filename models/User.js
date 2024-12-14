dd47c35dc9a1fc5bda998d4059a0543df4d66efc
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2024-12-14 13:10:25 +0600

    Add user model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+    const mongoose = require("mongoose");
+      const bcrypt = require("bcrypt");
+
+    const userSchema = new mongoose.Schema(
+      {
+        username: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String },
+        googleId: { type: String },
+        facebookId: { type: String }
+      },
+      { timestamps: true }
+    );
+
+ 
+
+    module.exports = mongoose.model("User", userSchema);
